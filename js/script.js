15d93ba6b7c4b06609cb8786bd1befabf856dfdd
Helen Keeling <helenlkeeling@gmail.com>
2018-12-16 19:46:24 +0000

    Fixed JS

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", (function() {
   //change the integers below to match the height of your upper dive, which I called
   //banner.  Just add a 1 to the last number.  console.log($(window).scrollTop())
   //to figure out what the scroll position is when exactly you want to fix the nav
   //bar or div or whatever.  I stuck in the console.log for you.  Just remove when
   //you know the position.
-  $(window).scroll(function () { 
+  window.scroll(function () { 
 
-    console.log($(window).scrollTop());
+    console.log(window.scrollTop());
 
-    if ($(window).scrollTop() > 550) {
-      $('#navbar').addClass('navbar-fixed-top');
+    if (window.scrollTop() > 550) {
+      document.getElementsByClassName('navbar').addClass('navbar-fixed-top');
     }
 
-    if ($(window).scrollTop() < 551) {
-      $('#navbar').removeClass('navbar-fixed-top');
+    if (window.scrollTop() < 551) {
+      document.getElementsByClassName('navbar').removeClass('navbar-fixed-top');
     }
   });
-});
+}));
 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+  var x = document.getElementById("mynavbar");
+  if (x.className === "navbar") {
+    x.className += " responsive";
+  } else {
+    x.className = "navbar";
+  }
+}
