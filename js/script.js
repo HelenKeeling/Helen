22cdb187f2a1eb26d60e5c5d021ca240e2083585
HelenKeeling <helenlkeeling@gmail.com>
2021-08-06 19:25:58 +0100

    re wrote contact form with validation

--- js/script.js
+++ js/script.js
@@ -1,60 +1,18 @@
-document.addEventListener("DOMContentLoaded", function () {
+// Contact form client side validation
+const email = document.getElementById("mail");
 
-    window.scroll(function () {
-        console.log(window.scrollTop());
-
-        if (window.scrollTop() > 550) {
-            document.getElementsByClassName("navbar").addClass("navbar-fixed-top");
-        }
-
-        if (window.scrollTop() < 551) {
-            document.getElementsByClassName("navbar").removeClass("navbar-fixed-top");
-        }
-    });
+email.addEventListener("input", function () {
+    if (email.validity.typeMismatch) {
+        email.setCustomValidity("I am expecting an e-mail address :)");
+    } else {
+        email.setCustomValidity("");
+    }
 });
 
-window.onload = function () {
-    var form = document.getElementById("formid");
-    form.onsubmit = submitted.bind(form);
-};
-
-function submitted(event) {
-    event.preventDefault();
-
-    var formContent = getFormContent();
-
-    var formContentAsJson = JSON.stringify(formContent);
-
-    var xmlhttp = new XMLHttpRequest();
-    xmlhttp.open(
-        "POST",
-        "https://rcf5xftu7d.execute-api.eu-west-1.amazonaws.com/prod/contact"
-    );
-    xmlhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-
-    xmlhttp.onload = function (e) {
-        console.log(xmlhttp.status);
-
-        if (xmlhttp.status === 200) {
-            alert("Thanks!!");
-        } else {
-            alert("Opps, try again");
-        }
-    };
-
-    xmlhttp.send(formContentAsJson);
-}
-
-function getFormContent() {
-    var firstNameValue = document.getElementById("fname").value;
-    var lastNameValue = document.getElementById("lname").value;
-    var emailValue = document.getElementById("ename").value;
-    var subjectValue = document.getElementById("subject").value;
-
-    return {
-        firstName: firstNameValue,
-        lastName: lastNameValue,
-        email: emailValue,
-        message: subjectValue
-    };
+function validateForm() {
+    let formValidation = document.forms["contact"]["name"]["email"]["message"].value;
+    if (formValidation == "") {
+        alert("The box must be filled out");
+        return false;
+    }
 }
